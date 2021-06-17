5402fa18f4dbe5e9612a0700fb5cc446d84866d9
thatguysilva <83712001+thatguysilva@users.noreply.github.com>
2021-06-17 14:45:45 -0300

    Add files via upload

--- myblog/views.py
+++ myblog/views.py
@@ -7,7 +7,7 @@ class HomeView(ListView):
 	queryset = Post.objects.order_by('-created_on')
 	model = Post
 	template_name ='home.html'
-	paginate_by = 1
+	paginate_by = 5
 	
 class ArticleDetailView(DetailView):
 	model = Post
@@ -15,3 +15,7 @@ class ArticleDetailView(DetailView):
 
 def AboutDetailView(request):
 	return render(request, 'about_details.html')
+	
+class MyListView(ListView):
+	model = Post
+	template_name = 'list_details.html'
