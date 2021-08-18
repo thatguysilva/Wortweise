0952c0b800cec864752a998f6ececc9dc58b8c3b
Alisson Silva <83712001+thatguysilva@users.noreply.github.com>
2021-08-18 16:29:00 -0300

    Add files via upload

--- myblog/forms.py
+++ myblog/forms.py
@@ -1,8 +1,29 @@
 from .models import Comment
 from django import forms
+from django.forms import TextInput
+
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('name', 'body')
+        widgets = {
+            'name': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Name'
+                }),
+            'body': TextInput(attrs={
+                'class': "form-control", 
+                'placeholder': 'Your message'
+                })
+        }
+ 
+ 
+    def __init__(self, *args, **kwargs):
+    	super(CommentForm, self).__init__(*args, **kwargs) # Call to ModelForm constructor
+    	self.fields['name'].widget.attrs['style'] = 'width:400px; height:25px;'
+    	self.fields['body'].widget.attrs['style']  = 'width:700px; height:100px;'
+
+
