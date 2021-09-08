a4cc57099d5ca087f51bcc74cb1eeb04930a4d34
Alisson Silva <83712001+thatguysilva@users.noreply.github.com>
2021-09-08 16:09:21 -0300

    Update settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -77,13 +77,15 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dchlun1cnkqi6n',
+        'USER': 'umcjlbudlavnfb'
+        'PASSWORD': '863a1056961f83bb75ca1a0eb38e6a5c11737ce9dd01522f8f09f1b38cd3e326',
+        'HOST': 'ec2-44-197-40-76.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
